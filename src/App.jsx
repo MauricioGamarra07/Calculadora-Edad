6a033957401e7b767676571b642ec5863611b468
MauricioGamarra07 <mauriciogamarra01@gmail.com>
2023-05-24 23:24:02 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,33 @@ function App() {
         <div className="cajastexto">
           <div className="caja-day">
             <label for="day">DAY</label>
-            <input type="number" id='day' />
+            <input type="text" id='day' placeholder='DD' />
           </div>
           <div className="caja-month">
             <label for="month">MONTH</label>
-            <input type="number" id='month' />
+            <input type="text" id='month' placeholder='MM' />
           </div>
           <div className="caja-year">
             <label for="year">YEAR</label>
-            <input type="number" id='year' />
+            <input type="text" id='year' placeholder='YYYY' />
           </div>
         </div>
         <div className="cont-medio">
           <div className="linea"></div>
-          <button id='btnFlecha'>I</button>
+          <i className='bx bxs-chevrons-down bx-lg flecha'></i>
         </div>
         <div className="resultado">
           <div className="year">
-
+            <h1 id='num'>--</h1>
+            <h1>years</h1>
           </div>
           <div className="month">
-
+            <h1 id='num'>--</h1>
+            <h1>months</h1>
           </div>
           <div className="day">
-
+            <h1 id='num'>--</h1>
+            <h1>days</h1>
           </div>
         </div>
       </div>
