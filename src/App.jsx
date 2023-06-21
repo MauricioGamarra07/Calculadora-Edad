bc9dc0e51d3d8f6ebf7226a9c35a856189d01992
MauricioGamarra07 <mauriciogamarra01@gmail.com>
2023-06-20 23:26:36 -0500

    desarrollando la logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-
+import { useState } from 'react'
 import './App.css'
+import Resultado from './assets/components/Resultado';
 
 function App() {
+
+  const [dia, setDia] = useState(0);
+  const [mes, setMes] = useState(0);
+  const [anio, setAnio] = useState(0);
+
+  function calcularEdad() {
+    const fecha = new Date();
+
+    let diaAct = fecha.getDate();
+    console.log("Día Actual: " + diaAct);
+    let mesAct = fecha.getMonth() + 1;
+    console.log("Mes Actual: " + mesAct);
+    let anioAct = fecha.getFullYear();
+    console.log("Año Actual: " + anioAct);
+
+    let subAnio = anioAct - anio;
+    let anioEdad = 0;
+    if(mesAct < mes){
+      anioEdad = subAnio - 1;
+    }else if(mesAct > mes){
+      anioEdad = subAnio;
+    }else if(mesAct == mes && diaAct > dia){
+      anioEdad = subAnio;
+    }else if(mesAct == mes && diaAct < dia){
+      anioEdad = subAnio - 1;
+    }
+    console.log(anioEdad);
+  }
 
   return (
 
@@ -9,36 +38,38 @@ function App() {
       <div className="card">
         <div className="cajastexto">
           <div className="caja-day">
-            <label for="day">DAY</label>
-            <input type="text" id='day' placeholder='DD' />
+            <p>DAY</p>
+            <input type="text" id='day' placeholder='DD' onChange={(e) => {
+              /* console.log(e.target.value); */
+              setDia(e.target.value);
+            }} />
           </div>
           <div className="caja-month">
-            <label for="month">MONTH</label>
-            <input type="text" id='month' placeholder='MM' />
+            <p>MONTH</p>
+            <input type="text" id='month' placeholder='MM' onChange={(e) => {
+              /* console.log(e.target.value); */
+              setMes(e.target.value);
+            }} />
           </div>
           <div className="caja-year">
-            <label for="year">YEAR</label>
-            <input type="text" id='year' placeholder='YYYY' />
+            <p>YEAR</p>
+            <input type="text" id='year' placeholder='YYYY' onChange={(e) => {
+              /* console.log(e.target.value); */
+              setAnio(e.target.value);
+            }} />
           </div>
         </div>
         <div className="cont-medio">
           <div className="linea"></div>
-          <i className='bx bxs-chevrons-down bx-lg flecha'></i>
+          <i className='bx bxs-chevrons-down bx-lg flecha' onClick={() => {
+            /* console.log("Hola Mundo"); */
+            console.log(dia);
+            console.log(mes);
+            console.log(anio);
+            calcularEdad();
+          }}></i>
         </div>
-        <div className="resultado">
-          <div className="year">
-            <h1 id='num'>--</h1>
-            <h1>years</h1>
-          </div>
-          <div className="month">
-            <h1 id='num'>--</h1>
-            <h1>months</h1>
-          </div>
-          <div className="day">
-            <h1 id='num'>--</h1>
-            <h1>days</h1>
-          </div>
-        </div>
+        <Resultado/>
       </div>
     </div>
   )
---
+++ src/assets/components/Resultado.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Resultado() {
+    return (
+        <>
+            <div className="resultado">
+                <div className="year">
+                    <h1 id='num'>--</h1>
+                    <h1>years</h1>
+                </div>
+                <div className="month">
+                    <h1 id='num'>--</h1>
+                    <h1>months</h1>
+                </div>
+                <div className="day">
+                    <h1 id='num'>--</h1>
+                    <h1>days</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Resultado
